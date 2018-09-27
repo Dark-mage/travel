8b234490fc0ca6f2aba0e831dd69d93a81cfec76
wf <wufeng8793@163.com>
2018-09-27 22:38:39 +0800

    项目初始化

---
+++ src/pages/home/home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>this is home</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  },
+  mounted () {},
+  methods: {}
+}
+</script>
+<style >
+</style>
